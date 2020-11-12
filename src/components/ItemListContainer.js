05c4864c2ae4d3b599b32c815cf0af09c46f14c0
Alejo Z <zitoalejo@gmail.com>
2020-11-11 21:54:09 -0300

    Agrego skeleton en el loading de item list

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,28 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import ItemList from './ItemList';
 import data from '../mockData/itemData.json'
+import { CircularProgress } from '@material-ui/core';
+import ItemListSkeleton from './ItemListSkeleton';
 
 //Fetch data promise
 const getItems = () => {
     return new Promise((res, rej) => {
-        setTimeout(() => res(data), 0);
+        setTimeout(() => res(data), 2000);
     });
 }
 
 function ItemListContainer({ title, onAdd }) {
 
+    const [loading, setLoading] = useState(false);
     const [itemList, setItemList] = useState([])
 
     useEffect(() => {
+        setLoading(true);
         getItems().then(result => {
             setItemList(result);
         }, err => {
             console.log(err);
-        })
-    },[]);
+        }).finally(result => {
+            setLoading(false);
+        });
+    }, []);
 
     return (
-        <ItemList title="Productos" itemDataList={itemList} onAdd={onAdd} />
+        <>
+            { loading ? <ItemListSkeleton/> : <ItemList title="Productos" itemDataList={itemList} onAdd={onAdd} />}
+        </>
     );
 }
 
