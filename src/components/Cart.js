9d64762271b8c0fce8139e1d20ee4d89d24de2fc
Alejo Z <zitoalejo@gmail.com>
2020-11-18 22:26:24 -0300

    Agrego new order en Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,28 +1,59 @@
 import React, { useEffect, useState } from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Checkbox from '@material-ui/core/Checkbox';
-import Avatar from '@material-ui/core/Avatar';
-import data from '../mockData/itemData.json'
-import { useCartContext } from '../context/cartContext';
-import { IconButton, Button, Box, Grid, Paper } from '@material-ui/core';
-import DeleteIcon from '@material-ui/icons/Delete'
-import { Link } from 'react-router-dom';
+import firebase from 'firebase'
+import '@firebase/firestore';
+import { Button, Grid, Paper } from '@material-ui/core';
 import CartItemList from './CartItemList'
+import { getFirestore } from '../firebase';
+import { useCartContext } from '../context/cartContext';
 
 
 export default function Cart() {
 
-    const styles ={
-        cartListContainer:{
+    const { itemsInCart, cartTotal } = useCartContext();
+
+    async function createOrder() {
+        const newOrder = {
+            buyer: { name: 'Alejo', phone: '+1154645544', email: 'test@mail.com' },
+            items: itemsInCart.map(item => ({ id: item.id, quantity: item.quantity, price: item.price })),
+            total: cartTotal,
+            date: firebase.firestore.FieldValue.serverTimestamp(),
+        };
+
+        const db = getFirestore();
+        const batch = db.batch();
+
+        //Get a firestore items
+        const itemQueryByManyIds = await db.collection('items')
+            .where(firebase.firestore.FieldPath.documentId(), 'in', itemsInCart.map(item=>item.id))
+            .get();
+        //const items = itemQueryByManyIds.docs.map(d => ({ id: d.id, ...d.data() }));
+
+        const itemDoc = itemQueryByManyIds.docs[0];
+        const res = await itemDoc.ref.update({ stock: itemDoc.data().stock - 1})
+
+        // itemQueryByManyIds.docs.forEach(itemDoc => {
+        //     itemDoc.update({ stock: itemDoc.data().stock - 1}) //toDo menos la cantidad del carrito
+        // });
+
+        const orders = db.collection('orders');
+
+        //batch.update(doc1, { field: 'newField' })
+
+        // try {
+        //     const { id } = await orders.add(newOrder);
+        //     console.log('Order created with id: ' + id);
+        // } catch (error) {
+        //     console.log('error creating order');
+        //     console.log(error);
+        // }
+    }
+
+    const styles = {
+        cartListContainer: {
             minHeight: '60vh',
             padding: 20,
             marginTop: 20,
-            display:'flex',
+            display: 'flex',
             flexDirection: 'column',
             justifyContent: 'space-between',
         }
@@ -37,6 +68,9 @@ export default function Cart() {
             <Grid item xs={12} sm={8}>
                 <Paper elevation={3} style={styles.cartListContainer}>
                     <CartItemList />
+                </Paper>
+                <Paper>
+                    <Button onClick={createOrder}>Crear orden</Button>
                 </Paper>
             </Grid>
         </Grid>
